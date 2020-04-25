0b96f42e5326918ba643db961287481359fbab0f
Alex Fargo <me@alexfargo.xyz>
2020-04-24 18:41:17 -0700

    initial commit

---
+++ Turnip Stalks/Sources/Screens/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Turnip Stalks
+//
+//  Created by Alex Fargo on 4/24/20.
+//  Copyright © 2020 Alex Fargo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
---
+++ Turnip StalksTests/Turnip_StalksTests.swift
@@ -0,0 +1,34 @@
+//
+//  Turnip_StalksTests.swift
+//  Turnip StalksTests
+//
+//  Created by Alex Fargo on 4/24/20.
+//  Copyright © 2020 Alex Fargo. All rights reserved.
+//
+
+import XCTest
+@testable import Turnip_Stalks
+
+class Turnip_StalksTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
